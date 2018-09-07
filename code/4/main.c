68bf96d9dffe86e82f2112348aa7e49fd353b3bb
Unknown <helpu1409@gmail.com>
2018-09-07 11:20:24 -0500

    agregando archivos

--- code/4/main.c
+++ code/4/main.c
@@ -6,6 +6,20 @@
 #include "tests.h"
 
 
+/*
+
+EXPRESIONES EMPLEADAS PARA HACER debug.
+
+print est
+print *((estudiante *)grupo->data)
+print *((estudiante *)grupo->next->data)
+print *((estudiante *)grupo->next->next->data)
+print *((estudiante *)data)
+print *((estudiante *)list->data)
+print *((estudiante *)rover->data)
+
+*/
+
 SListEntry *grupo = NULL;
 estudiante e_i_data;
 
@@ -15,7 +29,34 @@ int main() {
     printf("Ensayo\n");
     test1();
   */
-  /*Variables locales */
+  test1();
+  /*
+  bug: ESTE PROGRAMA NO FUNCIONA COMO SE QUISIERA, PRESENTA UN PROBLEMA
+  CUANDO SE AGREGA UN ELEMENTO A UNA LISTA EMPLEANDO UNA MISMA VARIABLE.
+
+  estudiante e1;
+  e1 = llenarDatos("Bart Simpson", "0001", 2.3);
+  imprimirDatos(e1);
+  slist_append(&grupo, &e1);
+  e1 = llenarDatos("Rafa Gorgori", "0002", 2.4);
+  imprimirDatos(e1);
+  slist_append(&grupo, &e1);
+  e1 = llenarDatos("Lisa Simpson", "0003", 5.0);
+  imprimirDatos(e1);
+  slist_append(&grupo, &e1);
+  imprimirLista();
+
+  */
+
+  /*
+
+  bug: ESTE PROGRAMA NO FUNCIONA COMO SE QUISIERA, PRESENTA UN PROBLEMA
+  CUANDO SE AGREGA UN ELEMENTO A UNA LISTA EMPLEANDO UNA MISMA VARIABLE.
+  CUANDO SE EMPLEAN VARIABLES DIFERENCTEA COMO ENTRADAS HAY SI DA COMO SE
+  PUDO EVIDENCIAR EN EL TEST, SIN EMBARGO PARA ESTE CASO NO TUVIMOS EXITO
+  POR ELLO DECIDIMOS COMENTAR ESTAS LINEAS HASTA DAR SOLUCION AL PROBLEMA.
+
+
   int opc;  // Variable asociada al menu
   int banExit = 0; // bandera para salirse del programa
   char nom[TAM_NAME]; // Nombres del estudiante
@@ -24,6 +65,7 @@ int main() {
   //int banEst;  // Bandera con la que se indica si un estudiante fue agregado a la lista
   estudiante *ptr;
   printf("%s\n","SISTEMA PARA ADMINISTRACION DEL CURSO DE SO");
+
   for(;;) {
     menu();
     scanf("%d", &opc);
@@ -44,9 +86,6 @@ int main() {
       case 2:
         printf("Ingresando los datos del estudiante\n");
         printf("- Nombre: ");
-        //gets(nom);
-        //fgets(nom, TAM_NAME, stdin);
-        //scanf("%[^\n]s", nom);  // Para poder leer espacios en el scanf
         scanf("%s", nom); // Nota: Hay un bug pues no se logran leer nombres completos
         printf("- Cedula: ");
         scanf("%s", cc);
@@ -54,13 +93,11 @@ int main() {
         if(slist_append(&grupo, &est) != NULL) {
           printf("\nEstudiante agregado...");
         }
-        else {
-          printf("\nERROR!!! No hay cupos...");
-        }
         printf("\n\nPresione [Enter] para continuar... ");
         waitForEnter();
         clear();
         break;
+
       case 3:
         printf("Ingrese la identificacion del estudiante a eliminar: ");
         scanf("%s",cc);
@@ -112,6 +149,7 @@ int main() {
         waitForEnter();
         clear();
         break;
+
       case 7:
         printf("Suerte es que le digo\n");
         banExit = 1;
@@ -124,8 +162,10 @@ int main() {
     if(banExit == 1) {
       break;
     }
+
   }
   printf("\nCerrando el programa...\n");
   waitForEnter();
+  */
   return 0;
 }
